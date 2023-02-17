7a19d09e549f74447fd3e1d459e74cceb9311888
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-02-17 21:44:16 +0530

    Added: Creation of a Stack

---
+++ src/com/datastruture/CreateStack.java
@@ -0,0 +1,15 @@
+package com.datastruture;
+
+import java.util.Stack;
+
+public class CreateStack {
+    public static void main(String[] args) {
+        Stack<Integer>stack = new Stack<>();
+
+        stack.push(56);
+        stack.push(30);
+        stack.push(70);
+        System.out.println("Element in stack:" + stack);
+
+    }
+}
